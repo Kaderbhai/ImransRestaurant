ada0fd0a588226c3c35522813fe8d9728f59379f
Imran Kaderbhai <imrankaderbhai@googlemail.com>
2016-08-30 22:51:10 +0100

    Coded the HTML

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="/assets/styles.css" rel="stylesheet">
+</head>
+
+<body id="final-example">
+	
+	<div class="wrapper">
+	
+		<div id="banner">
+
+			<img src="img/banner.png" alt="Immys">
+		</div><!-- banner -->
+		
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="menu.php">Menu</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div><!-- nav -->
+		
+		<div class="content">
+		
+			<div id="footer" class="cf">
+			
+				<div class="column three">
+					<strong>Phone</strong>
+					808.529.3819
+				</div><!-- column -->
+				
+				<div class="column three">
+					<strong>Location</strong>
+					Imrans Restaurant, Mytical Street, Aberystywth, SY23 3PP
+				</div><!-- column -->
+				
+				<div class="column three last">
+					<strong>Hours</strong><br>
+					<em>Monday - Saturday</em><br>
+					11:00am-9.00pm <br>
+					
+					<em>Sunday</em> <br>
+					5:00pm-9.00pm <br>
+				</div><!-- column -->
+			
+			</div><!-- footer -->
+			
+				<small>&copy;2016 SeyKad</small>
+		
+		</div><!-- content -->
+	
+	</div><!-- wrapper -->
+	
+	<div class="copyright-info">
+		<?php include('../assets/includes/copyrights.php'); ?>
+	</div>
+	
+</body>
+</html>
